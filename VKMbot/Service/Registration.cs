86f6e07b4a437abebc0e8a27c1597ce6d0bcfc82
Tohirjon-Odilov <shahanshoh819@gmail.com>
2024-02-02 12:31:19 +0500

    update files

--- VKMbot/Service/Registration.cs
+++ VKMbot/Service/Registration.cs
@@ -9,8 +9,8 @@
 //        //var checkNumber = new
 //        public Registration()
 //        {
-            //var checkNumber = new Random().Next(9999);
-            //Console.WriteLine(checkNumber);
+//var checkNumber = new Random().Next(9999);
+//Console.WriteLine(checkNumber);
 //            string accountSid = Environment.GetEnvironmentVariable("AC5c4fef9ad2fb254343f473130be3de2a");
 //            string authToken = Environment.GetEnvironmentVariable("50dc436cc4b61430f744fe7667716dce");
 
@@ -43,3 +43,21 @@
 //        }
 //    }
 //}
+
+
+//email
+//Random random = new Random();
+//int otp = random.Next(100000, 999999);
+//possword = otp.ToString();
+//var email = new MimeMessage();
+
+//email.From.Add(MailboxAddress.Parse("samadovxusan9013@gmail.com"));
+//email.To.Add(MailboxAddress.Parse(emailSender));
+//email.Subject = "Your verification code";
+//email.Body = new TextPart(TextFormat.Html) { Text = "Your verification code is " + otp };
+
+//var smpt = new SmtpClient();
+//await smpt.ConnectAsync("smtp.gmail.com", 587, SecureSocketOptions.StartTls);
+//await smpt.AuthenticateAsync("samadovxusan9013@gmail.com", "dixaxlwqasgetqlb");
+//await smpt.SendAsync(email);
+//await smpt.DisconnectAsync(true);
